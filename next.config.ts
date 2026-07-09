d4fd2d826dbe9906e17d61c4c273ed6c31e64ac7
Amar <ambir0513@gmail.com>
2026-07-10 02:50:31 +0530

    feat: add blogs app

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,34 @@
 import type { NextConfig } from "next";
 
-const nextConfig = {
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
+      // Notion uploaded images
       {
-        protocol: 'https',
-        hostname: 'pub-b289a32534284964b865f90fc9d138d6.r2.dev',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "prod-files-secure.s3.us-west-2.amazonaws.com",
+        pathname: "/**",
+      },
+
+      // Cloudflare R2
+      {
+        protocol: "https",
+        hostname: "pub-b289a32534284964b865f90fc9d138d6.r2.dev",
+        pathname: "/**",
+      },
+
+      // Cloudinary
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
+
+      // Images from your own domain
+      {
+        protocol: "https",
+        hostname: "amarbiradar.me",
+        pathname: "/**",
       },
     ],
   },
